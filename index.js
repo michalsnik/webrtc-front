7f3b2d82c19473afa5665cee062c03a7271d5ac6
Michał Sajnóg <michal.sajnog@netguru.co>
2017-03-17 00:39:05 +0100

    Example webrtc front

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var config = require('./config/environment');
+
+global.ENV = config(process.env.NODE_ENV);
+
+var app = express();
+
+app.use(bodyParser.text({ type: 'text/html' }))
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(ENV.PORT, function() {
+  console.log('WebRTC front is listening on port ' + ENV.PORT);
+});
